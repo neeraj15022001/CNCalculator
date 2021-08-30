8cd4d83c08fc931939df40b6c989e3bef6d65f15
neeraj15022001 <gneeraj32595@gmai.com>
2021-08-30 15:07:05 +0530

    added sounds to cache

--- sw.js
+++ sw.js
@@ -41,4 +41,4 @@ self.addEventListener("push", (event) => {
 // Pre Caching resources
 
 const cacheName = "cache-v1";
-const resourcesToPrecache = ["/", "/index.html"];
+const resourcesToPrecache = ["/", "/index.html", "/css/style.css", "/js/main.js", "/click.wav", "/toggle.wav", "/logo.png"];
